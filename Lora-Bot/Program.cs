ed9ec157561eb2cbfa424ce1cfbf66d67db465cf
BlubbFish <git@blubbfish.net>
2018-06-25 20:44:24 +0000

    [NF] Lora

---
+++ Lora-Bot/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Fraunhofer.Fit.Iot.Lora;
+
+namespace LoraDisplay {
+  class Program {
+    static void Main(String[] args) {
+      LoraController l = new LoraController();
+    }
+  }
+}
