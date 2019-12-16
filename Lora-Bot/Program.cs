210ed7817dacef7a6a1709465599c3c32d8141e2
Philip Schell <philip.schell@fit.fraunhofer.de>
2019-12-16 14:43:47 +0100

    Remove CMD Args and make debugging easyer

--- Lora-Bot/Program.cs
+++ Lora-Bot/Program.cs
@@ -8,21 +8,18 @@ using Fraunhofer.Fit.Iot.Lora.Events;
 namespace Fraunhofer.Fit.IoT.Bots.LoraBot {
   class Program : Bot<LoraController> {
     static void Main(String[] args) => new Program(args);
-    public Program(String[] args) {
+    public Program(String[] _) {
       InIReader.SetSearchPath(new List<String>() { "/etc/lorabot", Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData) + "\\lorabot" });
-      CmdArgs.Instance.SetArguments(new Dictionary<String, CmdArgs.VaildArguments>() {
-        { "-freq", new CmdArgs.VaildArguments(CmdArgs.ArgLength.Touple) },
-        { "-bw", new CmdArgs.VaildArguments(CmdArgs.ArgLength.Touple) },
-        { "-sp", new CmdArgs.VaildArguments(CmdArgs.ArgLength.Touple) },
-        { "-cr", new CmdArgs.VaildArguments(CmdArgs.ArgLength.Touple) },
-      }, args);
-      if (!CmdArgs.Instance.HasArgumentType("-freq") && !CmdArgs.Instance.HasArgumentType("-bw") && !CmdArgs.Instance.HasArgumentType("-sp") && !CmdArgs.Instance.HasArgumentType("-cr")) {
-        if (!InIReader.ConfigExist("settings")) {
-          Helper.WriteError("No settings.ini found. Abord!");
-          return;
-        }
-        InIReader settings = InIReader.GetInstance("settings");
-        this.logger.SetPath(settings.GetValue("logging", "path"));
+      if (!InIReader.ConfigExist("settings")) {
+        Helper.WriteError("No settings.ini found. Abord!");
+        return;
+      }
+      InIReader settings = InIReader.GetInstance("settings");
+      this.logger.SetPath(settings.GetValue("logging", "path"));
+      if (settings.GetValue("lora","debug") == "true") {
+        LoraController lora = new LoraController(settings.GetSection("lora"), false);
+        lora.Dispose();
+      } else {
         LoraController lora = new LoraController(settings.GetSection("lora"));
         this.ModulLoader("Fraunhofer.Fit.IoT.Bots.LoraBot.Moduls", lora);
         this.ModulInterconnect();
@@ -36,15 +33,6 @@ namespace Fraunhofer.Fit.IoT.Bots.LoraBot {
         Console.WriteLine("after dispose");
         lora.Dispose();
         Console.WriteLine("after loradisp");
-      } else if(CmdArgs.Instance.HasArgumentType("-freq") && CmdArgs.Instance.HasArgumentType("-bw") && CmdArgs.Instance.HasArgumentType("-sp") && CmdArgs.Instance.HasArgumentType("-cr")) {
-        LoraController lora = new LoraController(new Dictionary<String, String>() {
-          { "frequency", CmdArgs.Instance.GetArgumentData("-freq") },
-          { "signalbandwith", CmdArgs.Instance.GetArgumentData("-bw") },
-          { "spreadingfactor", CmdArgs.Instance.GetArgumentData("-sp") },
-          { "codingrate", CmdArgs.Instance.GetArgumentData("-cr") }
-        }, false);
-      } else {
-        Helper.WriteError("Usage for Debug:\n" + CmdArgs.Instance.GetUsageList("Lora-Bot"));
       }
     }
 
